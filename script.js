aaf4b7a92239a39ebfef45811376da6702e61e67
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-02 19:16:53 -0300

    Realizando os desafios 1 ao 6

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const colorBlack = document.querySelector('#black');
+const colorGreen = document.querySelector('#green');
+const colorBlue = document.querySelector('#blue');
+const colorYellow = document.querySelector('#yellow');
+
+let corSelecionada = 'cor';
+
+colorBlack.addEventListener('click', function () {
+  cor = 'black';
+});
+colorGreen.addEventListener('click', function () {
+  cor = 'green'
+});
+colorBlue.addEventListener('click', function () {
+  cor = 'blue';
+});
+colorYellow.addEventListener('click', function () {
+  cor = 'yellow';
+});
+//definindo a cor preta
+function backClassSelected(){
+    colorBlack.classList.remove('color');
+    colorBlack.classList.add('selected');
+    colorBlack.classList.add('color');
+}
+backClassSelected();
+
+//Selecionando uma cor da paleta
+function selectColor(selecionar){
+    let SelectElement = document.querySelector('.selected');
+    SelectElement.classList.remove('selected');
+    selecionar.target.classList.add('selected');
+ const botaoGrade = document.querySelector('.pixel');
+ botaoGrade.addEventListener('click', function(){
+   //for (let i = 0; i < botaoGrade.length; i +=1){
+    if(botaoGrade.style.backgroundColor === "white"){
+        botaoGrade.style.backgroundColor = 'selecionar';
+    }else {
+        botaoGrade.style.backgroundColor = 'white';
+    }
+   //}
+ });
+}
+//selectColor(selecionar);
+
+//criando grade pixel
+function criandoGradePixel(){
+const grade = document.querySelector('#pixel-board');
+
+    let n = 5;
+    for(let l = 0; l < n; l += 1){
+      for(let c = 0; c < n; c += 1){
+        const criandoGrade = document.createElement('div');
+        criandoGrade.classList.add('pixel');
+        grade.appendChild(criandoGrade);
+    }
+}
+}
+criandoGradePixel();
