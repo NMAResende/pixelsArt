0026db623140e10c49c4d78f8022777eb8cc1310
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-03 18:26:42 -0300

    Formatando arquivo

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const colorBlack = document.querySelector('#black');
 const colorGreen = document.querySelector('#green');
 const colorBlue = document.querySelector('#blue');
 const colorYellow = document.querySelector('#yellow');
-
+let corSelecionada = 'black';
 function colorirPixel(selecionou) {
   selecionou.style.backgroundColor = corSelecionada;
 }
 document.querySelectorAll('#pixel-board').forEach((div) => {
   div.addEventListener('click', (event) => {
-    let selecionou = document.querySelector('#' + event.target.id);
+    const selecionou = document.querySelector('#' + event.target.id);
     colorirPixel(selecionou);
   });
 });
@@ -17,14 +17,13 @@ document.querySelectorAll('#pixel-board').forEach((div) => {
 const criandoBotao = document.getElementById('clear-board');
 criandoBotao.addEventListener('click', function () {
   document.querySelectorAll('#pixel-board').forEach((div) => {
-    let filhos = div.children;
+    const filhos = div.children;
     for (let i = 0; i < filhos.length; i += 1) {
       filhos[i].style.backgroundColor = 'white';
     }
-  })
+  });
 });
 
-let corSelecionada = 'black';
 colorBlack.addEventListener('click', function () {
   corSelecionada = 'black';
 });
@@ -41,8 +40,8 @@ colorYellow.addEventListener('click', function () {
 function borda() {
   const bordaElementos = document.querySelectorAll('.color');
   for (let i = 0; i < bordaElementos.length; i += 1) {
-    bordaElementos[i].style.border = "1px solid black";
-  };
+    bordaElementos[i].style.border = '1px solid black';
+  }
 }
 borda();
 
@@ -74,7 +73,7 @@ selectColor();
 //criando grade pixel
 function criandoGradePixel() {
   const grade = document.querySelector('#pixel-board');
-  let n = 5;
+  const n = 5;
   let numId = 0;
   for (let l = 0; l < n; l += 1) {
     for (let c = 0; c < n; c += 1) {
