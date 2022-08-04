d60049d2900053e2a0a6a344aed804d61c52b113
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-03 22:23:28 -0300

    Realizando o oedesafio 10

--- script.js
+++ script.js
@@ -86,3 +86,29 @@ function criandoGradePixel() {
   }
 }
 criandoGradePixel();
+
+//desafio 10
+const botao = document.getElementById('generate-board');
+const input = document.querySelectorAll('input');
+botao.addEventListener('click', function (){
+  const valores = null;
+    input.forEach(item =>{
+      if (item.value)
+      valores.push(parseFloat(item.value));
+      alert("Board inválido!");
+    });
+ function changeSize (){
+  input.innerHTML = '';
+  for(let i = 0; input.length; i += 1){
+    input.innerHTML += 'pixel';
+    input.innerHTML= Math.pow('generate-board', 2);
+ }
+}
+changeSize();
+ });
+ 
+
+
+
+
+  
